9fb93324bf4c022fd733ba065c798225925158a9
ChristianClear <christian.clear@imperial.ac.uk>
2024-05-14 11:20:24 +0100

    feat: added spectrum output

--- opustonso.py
+++ opustonso.py
@@ -1,10 +1,12 @@
 import brukeropusreader
 import numpy as np
+import sys
 
-opus_data = brukeropusreader.read_file("FeAr_SP500.3")
+filename = sys.argv[1]
+opus_data = brukeropusreader.read_file(filename)
 
-with open('test.hdr', 'w') as hdr_file:
-    with open('test.dat', 'w') as dat_file:
+with open(filename+'.hdr', 'w') as hdr_file:
+    with open(filename+'.dat', 'w') as dat_file:
         
         ### File Parameters ###
         day = opus_data["IgSm Data Parameter"]['DAT']
@@ -24,7 +26,7 @@ with open('test.hdr', 'w') as hdr_file:
         ntrans = 2097152
         freespec = opus_data["Instrument"]['HFL'] 
         
-        ### Write to hdr file ###
+        ### Write interferogam header ###
         hdr_file.writelines(
             f'/         FILE PARAMETERS\n'
             f'id      = NSO type 6 FTS data file     \n'                                       
@@ -52,7 +54,41 @@ with open('test.hdr', 'w') as hdr_file:
             f'END'
             )
         
-        ### Write to dat file ###
+        ### Write interferogram dat file ###
         dat_data = np.array(opus_data["IgSm"], dtype=np.float32)  # convert interferogram datapoints to 32-bit floats
         dat_data.tofile(dat_file)  # write interferogram data to binary file
         
+with open(filename+'_r.hdr', 'w') as hdr_spe:
+    with open(filename+'_r.dat', 'w') as spe_file:
+
+        ### Write spectrum header ###
+        hdr_spe.writelines(
+            f'/         FILE PARAMETERS\n'
+            f'id      = NSO type 6 FTS data file     \n'                                       
+            f'day     = {day}        / Acquisition date \n'
+            f'wstart  = {wstart}              / First wavenumber / point in file\n'
+            f'wstop   = {wstop}         / Last wavenumber / point in file\n'
+            f'npo     = {npo}                / Number of points in file\n'
+            f'data_is = Real                 / Data ARE Real or Complex\n'
+            f'delw    = {delw}                 / Dispersion (1/cm / point)\n'
+            f'resolutn= {resolutn}               / Spectral resolution (1/cm)\n'
+            f'fboffs  = 0                     / File byte offset\n'
+            f'bocode  = 0                     / Data byte order: 0=little endian; 1=big endian\n'
+            f'xaxis_is= Index                 / Wavenumber, Wavelength, Index, Other\n'
+            f'/         SCAN PARAMETERS\n'
+            f'alias   = {alias}               / Alias\n'
+            f'fdr     = {fdr}                 / Samples per laser fringe (fringe div. ratio)\n'
+            f'drivevel= 2000.0                / Carriage velocity in fringes / sec\n'
+            f'refwavno= {refwavno}            / Laser wavenumber (1/cm)\n'
+            f'fclock  = {fclock}              / Sampling clock frequency (Hz)\n'
+            f'sampfreq= {sampfreq}            / Sampling frequency (Hz)\n'
+            f'/         TRANSFORM PARAMETERS\n'
+            f'nint    = {nint}                / Number of pts in interferogram\n'
+            f'ntrans  = {ntrans}               / Transform size (2**N)\n'
+            f'freespec= {freespec}          / Free spectral range (1/cm)\n'
+            f'END'
+            )
+        
+        ### Write spectrum .dat file ###
+        spe_data = np.array(opus_data["ScSm"], dtype=np.float32)  # convert spectrum datapoints to 32-bit floats
+        spe_data.tofile(spe_file)  # write spectrum data to binary file
